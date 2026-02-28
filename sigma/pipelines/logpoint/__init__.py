f4bb73531105b5d77424adeeeacc0455206ec5eb
vl43den <is231338@fhstp.ac.at>
2026-02-28 09:54:40 +0100

    feat: Add Linux pipeline with auditd field mappings and generic taxonomy

--- sigma/pipelines/logpoint/__init__.py
+++ sigma/pipelines/logpoint/__init__.py
@@ -1,9 +1,11 @@
 from .windows import logpoint_windows_pipeline
 from .m365 import logpoint_m365_pipeline
 from .azure import logpoint_azure_pipeline
+from .linux import logpoint_linux_pipeline
 
 pipelines = {
     "logpoint_windows": logpoint_windows_pipeline,
     "logpoint_o365": logpoint_m365_pipeline,
     "logpoint_azure": logpoint_azure_pipeline,
+    "logpoint_linux": logpoint_linux_pipeline,
 }
